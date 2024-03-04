b463df853bba2c61240a29a968a64739e8804b17
Sean Maruel A. Comingues <seanmaruel@Seans-Mac-mini.local>
2024-03-04 16:08:52 +0800

    scanner functionality

--- pages/private/Scan.js
+++ pages/private/Scan.js
@@ -1,8 +1,65 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button, TouchableOpacity } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
-const Scan = () => {
-    return ( <SafeAreaView><Text>Scan</Text></SafeAreaView> );
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(null);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(data);
+  };
+
+  if (hasPermission === null) {
+    return <View className=" w-full h-full items-center justify-center flex"><Text>Requesting for camera permission</Text></View>;
+  }
+  if (hasPermission === false) {
+    return <View className=" w-full h-full items-center justify-center flex"><Text>No Access To Camera</Text></View>;
+  }
+  if(scanned){
+    return (<View className=" w-full h-full items-center justify-center flex">
+        <Text>{scanned}</Text>
+        <View className=" flex flex-row items-center justify-center px-5 mt-5">
+        <TouchableOpacity onPress={() => setScanned(null)} className=" w-1/2 flex items-center justify-center rounded px-5 py-2 bg-cyan-600 text-white">
+            <Text className=" text-white text-base">Rescan</Text>
+        </TouchableOpacity>
+        </View>
+    </View>)
+  }
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+});
+// import { CameraView, useCameraPermissions } from 'expo-camera/next';
+// import { View } from 'react-native';
+
+// const Scan = () => {
+//     return ( <View>
+//         <CameraView  facing={"back"}>
+//         </CameraView>
+//     </View> );
+// }
  
-export default Scan;
+// export default Scan;
