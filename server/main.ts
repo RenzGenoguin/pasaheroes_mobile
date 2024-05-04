8a836e3503c02d63289a7a977f1aed99ec556422
zynmurel <comingues99@gmail.com>
2024-05-04 14:06:44 +0800

    url changes

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 //https://pasaheroes-web-liard.vercel.app/
 export const api = axios.create({ 
-  baseURL: 'http://192.168.0.113:3000/api/mobile_api/', //office ip address
-  // baseURL: 'https://pasaheroes-web-liard.vercel.app/api/mobile_api/', //vercel
+  // baseURL: 'http://192.168.0.113:3000/api/mobile_api/', //office ip address
+  baseURL: 'https://pasaheroes-web-liard.vercel.app/api/mobile_api/', //vercel
     timeout: 5000, // Set a timeout for requests (in milliseconds)
     headers: {
       'Content-Type': 'application/json',
