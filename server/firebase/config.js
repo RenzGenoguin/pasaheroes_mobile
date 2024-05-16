4a4b5b891ed27e28f4217a2012475024293b522a
renz <genoguinrenz@gmail.com>
2024-05-16 21:33:16 +0800

    change api key for firebase

--- server/firebase/config.js
+++ server/firebase/config.js
@@ -2,12 +2,13 @@ import firebase from "firebase/compat/app";
 import 'firebase/compat/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAovtURtW21Ij_uTNVAu-BGRKZxr9esDMk",
-  authDomain: "imageuploadforthesises.firebaseapp.com",
-  projectId: "imageuploadforthesises",
-  storageBucket: "imageuploadforthesises.appspot.com",
-  messagingSenderId: "255663088615",
-  appId: "1:255663088615:web:5b31db93cd972fdeadd342",
+  apiKey: "AIzaSyCtt3Z5XZW2EakSl6RrwSXdwLRk3F6nNMg",
+  authDomain: "pasaheroes-f1831.firebaseapp.com",
+  projectId: "pasaheroes-f1831",
+  storageBucket: "pasaheroes-f1831.appspot.com",
+  messagingSenderId: "1061524938923",
+  appId: "1:1061524938923:web:d29b44fad2e37e85f557ee",
+  measurementId: "G-W5DBVGJS53"
 };
 
 if(!firebase.apps.length){
